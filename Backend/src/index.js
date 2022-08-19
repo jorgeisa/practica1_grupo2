e3e033a126692334e80b79bf0fb8be150395f599
Alex <3032801820108@ingenieria.usac.edu.gt>
2022-08-19 15:24:35 -0600

    [ADD]bugfix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import indexRoutes from './routes/index.js'
-
+import cors from 'cors'
 const app = express()
-
+app.use(cors());
 // Settings
 app.set('port', 3000)
 
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+
 const router = Router()
 
 // Ruta Raiz
@@ -15,30 +16,39 @@ router.get('/', (req, res)=>{
 
 router.get('/PAROIMPAR/:numero',(req,res)=> {
     var response = req.params.numero;
+    if (!isNaN(response)){
     var resultado = Verificar(response);
     if (resultado == true){
         res.json({Resultado: 'El numero es par'})
-    }else {
+    }else if (resultado == false) {
         res.json({Resultado: 'El numero es impar'})
-    } 
+    }}else{
+        res.json({Resultado: 'No es un numero'})
+    }
 });
 
 
 router.get('/alreves/:palabra', function(req, res) { 
+    if (isNaN(req.params.palabra) ){
     let cadenaAlreves = alreves(req.params.palabra);
 
     res.json({ 
-        mensaje: req.params.palabra +' = ' +cadenaAlreves
-    })  
+        mensaje: cadenaAlreves
+    })  }else{
+        res.json({mensaje: 'No es una palabra'})
+    }
 })
 
 function Verificar(numero){
-    return (numero % 2) == 0;
+
+        return (numero % 2) == 0;
+
 }
 
 // Endpoint Fibonacci
 router.get('/fibo/:numero', (req, res)=> {
     let numero = req.params.numero
+    if (!isNaN(numero) ){
     var fibonacci_numero = fibonacci_func(numero);
     res.json(
         {
@@ -46,17 +56,23 @@ router.get('/fibo/:numero', (req, res)=> {
             Numero: numero,
             Fibonacci: fibonacci_numero
         }
-    )
+    ) }else{
+        res.json({Fibonacci: 'No es un numero'})
+    }
 })
 
 // FUncion Raiz
-app.get('/raiz/:numero', function(req, res) { 
+router.get('/raiz/:numero', function(req, res) { 
+    if (!isNaN(req.params.numero) ){
     let result_raiz = raiz(req.params.numero);
 
     res.json({ 
         mensaje: result_raiz
-    })  
+    })  }else{
+        res.json({mensaje: 'No es un numero'})
+    }
 })
+
 
 function raiz(numero){
     return Math.cbrt(numero)
@@ -64,11 +80,13 @@ function raiz(numero){
 
 //Funcion division
 router.get('/division/:numero1/:numero2', function(req, res) { 
+    if (!isNaN(req.params.numero1) && !isNaN(req.params.numero2)){
     let division = div(req.params.numero1,req.params.numero2);
-
     res.json({ 
         mensaje: division
-    })  
+    })  }else{
+        res.json({mensaje: 'No es un numero'})
+    }
 })
 
 function div(n1,n2){
@@ -94,22 +112,26 @@ function alreves(cadena){
     return resultado;
 }
 
-app.get('/potencia/:base', function(req, res) { 
+router.get('/potencia/:base', function(req, res) { 
     let base = req.params.base;
-
+    if (!isNaN(base) ){
     res.json({ 
         mensaje: Math.pow(base, 3)
-    })  
+    })  }else{
+        res.json({mensaje: 'No es un numero'})
+    }
 })
 
 
-app.get('/multiplicacion/:numero1/:numero2', function(req, res) { 
+router.get('/multiplicacion/:numero1/:numero2', function(req, res) { 
     let numero1 = req.params.numero1;
     let numero2 = req.params.numero2;
-
+    if (!isNaN(numero1) && !isNaN(numero2)){
     res.json({ 
         resultado:numero1*numero2
-    })  
+    })  }else{
+        res.json({resultado: 'No es un numero'})
+    }
 })
 
 
